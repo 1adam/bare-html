06d669ec9d19ebe52698460221ba72d5bb08c6a1
1adam <1adam@users.noreply.github.com>
2018-04-15 02:13:53 -0400

    tweaks

--- scripts/prepConfigs.sh
+++ scripts/prepConfigs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-mkdir /opt/application/conf/
+mkdir -p /opt/application/conf/
 if [[ "$DEPLOYMENT_GROUP_NAME" =~ "_prod" ]]; then
-  cp -r /tmp/application/configs/prod/ /opt/application/conf/
+  cp -r /tmp/configs/prod/* /opt/application/conf/
   sed -i "s/%APPENV%/PRODUCTION/g" /var/www/html/index.html
-elif [[ "$DEPLOYMENT_GROUP_NAME" =~ "_stg " ]]; then
-  cp -r /tmp/application/configs/staging/ /opt/application/conf/
+elif [[ "$DEPLOYMENT_GROUP_NAME" =~ "_stg" ]]; then
+  cp -r /tmp/configs/staging/* /opt/application/conf/
   sed -i "s/%APPENV%/STAGING/g" /var/www/html/index.html
 else
   echo "** prepConfigs.sh ** Something wrong with DEPLOYMENT_GROUP_NAME"
 fi
-rm -rf /tmp/application/
+rm -rf /tmp/configs
